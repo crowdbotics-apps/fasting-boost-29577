d212d3fa6df5d6672a847847730180a15b0f4c1e
Crowdbotics <team@crowdbotics.com>
2021-08-06 18:30:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fasting_boost_29577Identifier",
+  url: "https://fasting_boost_29577Identifier.botics.co",
+  api: "https://fasting_boost_29577Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
